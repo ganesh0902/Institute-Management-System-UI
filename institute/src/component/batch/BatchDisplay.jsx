0718591c44141dc9a91b19fa6378ee2d0c34b8e7
Ganesh sakhare <101263428+ganeshsakhare0902@users.noreply.github.com>
2024-05-20 10:36:54 +0530

    changes in batch display

--- institute/src/component/batch/BatchDisplay.jsx
+++ institute/src/component/batch/BatchDisplay.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useState } from 'react';
+import { Toast } from 'primereact/toast';
+import { useState,useRef } from 'react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { InputText } from "primereact/inputtext";
@@ -8,10 +9,16 @@ import 'primereact/resources/themes/saga-blue/theme.css'; // or any other theme 
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import { MultiSelect } from 'primereact/multiselect';
+import { RiLockPasswordFill } from "react-icons/ri";
 import {Button} from 'primereact/button'
 import { MdMarkEmailRead } from "react-icons/md";
+import { Dropdown } from 'primereact/dropdown';
 import { Avatar } from 'primereact/avatar';
+import { FileUpload } from 'primereact/fileupload';
 import { FaUserNurse } from "react-icons/fa";
+import 'primereact/resources/themes/saga-blue/theme.css';  // Import PrimeReact themes
+import 'primereact/resources/primereact.min.css';          // Import PrimeReact CSS
+import 'primeicons/primeicons.css';  
 
 const BatchDisplay=()=>{
     
@@ -22,6 +29,8 @@ const BatchDisplay=()=>{
     const[course,setCourse]=useState([]);
     const[teacher,setTeacher]=useState([]);
     const [selectedSkill, setSelectedSkills] = useState(null);
+    const toast = useRef(null);
+    const [loading, setLoading] = useState(false);
     useEffect(()=>{
         
         fetch(`http://localhost:9002/batch/`+bId).then(async (result)=>{
@@ -41,6 +50,15 @@ const BatchDisplay=()=>{
         { name: 'Reactjs', id: 'PRS' }
     ];
 
+    const [selectedTeacher, setSelectedTeacher] = useState(null);
+    const teachers = [
+        { name: 'New York', code: 'NY' },
+        { name: 'Rome', code: 'RM' },
+        { name: 'London', code: 'LDN' },
+        { name: 'Istanbul', code: 'IST' },
+        { name: 'Paris', code: 'PRS' }
+    ];
+
     useEffect(()=>{        
         const {course}=batch;
         setCourse(course);
@@ -51,22 +69,76 @@ const BatchDisplay=()=>{
     if (!course) {
         return <div>Loading...</div>; // Return loading indicator if course is not available yet
       }
-    
+
+      const onUpload = () => {
+        toast.current.show({ severity: 'info', summary: 'Success', detail: 'File Uploaded' });
+    };    
+
+    const load = () => {
+        setLoading(true);
+
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    };
+
     return(        
         <section className='batchDisplay'>
             <div className='container p-4'>
-                <div className='row'>
-                    <div className='col-12 col-md-6 col-sm-12 shadow py-5'>                        
-                        <div className='row'>
+                <div className='row'>                    
+                    <div className='col-12 col-md-6 col-sm-12 mt-2 shadow pt-1'>
+                        <div className='row'>                            
                             <div className='col-12 col-md-6 col-sm-12 mt-2'>
+                                <img className='batch-card card-img-top' loading='lazy' alt='uploading' src={`../batch/${batch.image}`}/>
+                            </div>
+                            <div className='col-12 col-md-6 col-sm-12 mt-2'>
+                                <label><MdMarkEmailRead/> </label> &nbsp;
+                                <small> {batch.batchTitle} </small> <br/>
+                                <label><FaUserNurse/></label> &nbsp; 
+                                <small>{batch.time} </small> &nbsp;&nbsp;&nbsp;
+                                <label><FaUserNurse/></label> &nbsp;
+                                <small>02:50 </small> <br/>                                                                
+                            </div>
+                            <div className='col-12 col-md-6 col-sm-12 mt-2'>
+                                <label htmlFor="username">Batch Name</label>
+                                <InputText id="username" readOnly="true" value={batch.batchTitle} className='form-control'/>                                                                           
+                            </div>
+                            <div className='col-12 col-md-6 col-sm-12 mt-2'>
+                                <label htmlFor="username"> Duration </label>
+                                <InputText id="username" className='form-control' readOnly="true" value={batch.duration}/>                                                                           
+                            </div>
+                            <div className='col-12 col-md-6 col-sm-12 mt-2'>
+                                <label htmlFor="username">STart Date</label>
+                                <InputText id="username" className='form-control' readOnly="true" value={batch.time}/>                                                                           
+                            </div>
+                            <div className='col-12 col-md-6 col-sm-12 mt-2'>
+                                <label htmlFor="username">End Date</label>
+                                <InputText id="username" className='form-control' readOnly="true" value='02:30'/>                                                                           
+                            </div>
+                            <div className='col-12 col-md-6 col-sm-12 mt-2'>
+                                <label htmlFor="username">Location</label>
+                                <InputText id="username" className='form-control' readOnly="true" value={batch.location}/>                                                                           
+                            </div>                                                        
+                            <div className='col-12 col-md-6 col-sm-12 mt-2'>
+                                <label htmlFor="username">Update File </label>
+                                <FileUpload   
+                                onUpload={onUpload}                                                                                           
+                                className='fileUploaderBatch'                                 
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-12 col-md-6 col-sm-12 shadow pt-1'>                        
+                        <div className='row'>                            
+                            <div className='col-12 col-md-6 col-sm-12 mt-3'>
                                 <img className='w-100' src="https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"></img>
                             </div>
                             <div className='col-12 col-md-6 col-sm-12 mt-2'>
                                 <label><MdMarkEmailRead/> </label> &nbsp;
                                 <small> ganeshs2987@gmail.com </small> <br/>
                                 <label><FaUserNurse/></label> &nbsp;
-                                <small>Ganesh Sakhare</small> <br/>                                                               
-                            </div>
+                                <small>Ganesh Sakhare</small>                                                                                              
+                            </div>                            
                             <div className='col-12 col-md-6 col-sm-12 mt-2'>
                                 <label htmlFor="username">Course Name</label>
                                 <InputText id="username" className='form-control' readOnly="true" value='Java Development'/>                                                                           
@@ -80,6 +152,11 @@ const BatchDisplay=()=>{
                                 <InputText id="fees" variant="filled" className='form-control' readOnly="true" value='Rs 2000'/> 
                             </div>
                             <div className='col-12 col-md-6 col-sm-12 mt-2'>
+                                <label htmlFor="username">Select Teacher</label><br/>
+                                <Dropdown value={selectedTeacher} onChange={(e) => setSelectedTeacher(e.value)} options={teachers} optionLabel="name" 
+                                    placeholder="Select a City" style={{width:"100%"}} className="p-2"/>
+                            </div>
+                            <div className='col-12 col-md-6 col-sm-12 mt-2'>
                                 <label htmlFor="ms-cities">Select The Skills </label>
                                 <FloatLabel className="w-full md:w-26rem custom-floatlabel-batch mt-1 form-control">                                
                                 <MultiSelect value={selectedSkill} onChange={(e) => setSelectedSkills(e.value)} 
@@ -87,37 +164,12 @@ const BatchDisplay=()=>{
                                     panelClassName="custom-multiselect-panel-batch" style={{width:"auto"}} />                                
                                 </FloatLabel>
                             </div>
+                            <div className='col-12 col-md-6 col-sm-12 mt-4'>
+                                <Button label="Submit" className='btn px-2 form-control updateBatchBtn'  icon="pi pi-check" loading={loading} onClick={load} 
+                                />
+                            </div>
                         </div>                        
                     </div>                    
-                    <div className='col-12 col-md-6 col-sm-12 mt-2 shadow py-5'>
-                        <div className='row'>                            
-                            <div className='col-12 col-md-6 col-sm-12 mt-2'>
-                                <img className='w-100' src="https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"></img>
-                            </div>
-                            <div className='col-12 col-md-6 col-sm-12 mt-2'>
-                                <label><MdMarkEmailRead/> </label> &nbsp;
-                                <small> ganeshs2987@gmail.com </small> <br/>
-                                <label><FaUserNurse/></label> &nbsp;
-                                <small>Ganesh Sakhare</small> <br/>                                                               
-                            </div>
-                            <div className='col-12 col-md-6 col-sm-12 mt-2'>
-                                <label htmlFor="username">First Name</label>
-                                <InputText id="username" readOnly="true" value='Ganesh' className='form-control'/>                                                                           
-                            </div>
-                            <div className='col-12 col-md-6 col-sm-12 mt-2'>
-                                <label htmlFor="username">Last Name</label>
-                                <InputText id="username" className='form-control' readOnly="true" value='Sakhare'/>                                                                           
-                            </div>
-                            <div className='col-12 col-md-6 col-sm-12 mt-2'>
-                                <label htmlFor="username">Email</label>
-                                <InputText id="username" className='form-control' readOnly="true" value='ganeshs2987@gmail.com'/>                                                                           
-                            </div>
-                            <div className='col-12 col-md-6 col-sm-12 mt-2'>
-                                <label htmlFor="username">Contact</label>
-                                <InputText id="username" className='form-control' readOnly="true" value='9595956150'/>                                                                           
-                            </div>
-                        </div>
-                    </div>
                 </div>                    
             </div>
         </section>                                     
