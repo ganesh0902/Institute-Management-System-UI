5340be3d5b85ab2d9279182fd9b9353984d40037
Ganesh sakhare <101263428+ganeshsakhare0902@users.noreply.github.com>
2024-05-24 21:51:54 +0530

    changes

--- institute/src/component/batch/BatchDisplay.jsx
+++ institute/src/component/batch/BatchDisplay.jsx
@@ -18,6 +18,7 @@ import { FaUserNurse } from "react-icons/fa";
 import 'primereact/resources/themes/saga-blue/theme.css';  // Import PrimeReact themes
 import 'primereact/resources/primereact.min.css';          // Import PrimeReact CSS
 import 'primeicons/primeicons.css';  
+import axios from 'axios';
 
 const BatchDisplay=()=>{
     
@@ -42,16 +43,22 @@ const BatchDisplay=()=>{
     const[teacherId,setTeacherId]=useState(0);     
     const[time,setTime]=useState("10:30 PM");  
     
-    useEffect(()=>{
-        
+    useEffect(()=>{        
         fetch(`http://localhost:9002/batch/`+bId).then(async (result)=>{
 
             await result.json().then((response)=>{
-                setBatch(response);                                                      
+                setBatch(response);    
+                
+                batch.map((result)=>{
+                    setBatch(result.teacherId);
+                    setCourserId(result.courseId);
+                })
             })
         })
 
     },[bId]);
+
+
 
     const skillSet = [
         { name: 'Java', id: 'NY' },
@@ -86,35 +93,48 @@ const BatchDisplay=()=>{
     };    
 
     const makeReadOnly = () => {
-        setIsReadOnly(!isReadOnly);
-        alert("");
+        setIsReadOnly(!isReadOnly);        
     };
 
     const load = () => {
         setLoading(true);
-
+        //updateBatch();
         setTimeout(() => {
             setLoading(false);
+            
         }, 2000);
     };
+
+    const updateBatch=()=>{              
+
+        const batchDto={bId,batchTitle, duration, startDate, endDate, status, location, teacherId, courseId};
+
+        console.log(batchDto);
+
+        // const save_batch="http://localhost:9002/batch/";
+        //       const response=axios.post(`${save_batch}`,batchDto);              
+        //       alert("Batch Updated Successfully");
+        //       console.log(response.data);
+        //       console.log(batch);           
+    }
 
     return(        
         <section className='batchDisplay'>
             <div className='container p-4'>
                 <div className='row'>                    
                     <div className='col-12 col-md-6 col-sm-12 mt-2 shadow pt-1'>
-                        <div className='row'>                            
+                        <div className='row'>                                                    
                             <div className='col-12 col-md-6 col-sm-12 mt-2'>
                                 <img className='batch-card card-img-top' loading='lazy' alt='uploading' src={`../batch/${batch.image}`}/>
-                            </div>
+                            </div>                           
                             <div className='col-12 col-md-6 col-sm-12 mt-2 text-center'>
                                 <label><MdMarkEmailRead/> </label> &nbsp;
                                 <small> {batch.batchTitle} </small>  &nbsp;&nbsp;
                                 <i className="pi pi-pencil" onClick={()=>makeReadOnly()} style={{ fontSize: '1em',cursor:"pointer" }}></i>                                                                                           
-                            </div>
+                            </div>                            
                             <div className='col-12 col-md-6 col-sm-12 mt-2'>
                                 <label htmlFor="username">Batch Name</label>
-                                <InputText id="username" readOnly={isReadOnly} defaultValue={batch.batchTitle} onChange={(even)=>setBatchTitle(even.target.value)} className='form-control'/>                                                                           
+                                <InputText id="username" readOnly={isReadOnly} value={batch.batchTitle}  onChange={(even)=>setBatchTitle(even.target.value)} className='form-control'/>                                                                           
                             </div>
                             <div className='col-12 col-md-6 col-sm-12 mt-2'>
                                 <label htmlFor="username"> Duration </label>
@@ -146,7 +166,7 @@ const BatchDisplay=()=>{
                             </div>
                             <div className='col-12 col-md-6 col-sm-12 mt-4'>
                                 {   !isReadOnly &&
-                                    <Button label="Submit" className='btn px-2 form-control updateBatchBtn'  icon="pi pi-check" loading={loading} onClick={load} />                            
+                                    <Button label="Submit" className='btn px-2 form-control updateBatchBtn'  icon="pi pi-check" loading={loading} onClick={()=>updateBatch()} />                            
                                 }
                             </div>
                         </div>
@@ -154,7 +174,7 @@ const BatchDisplay=()=>{
                     <div className='col-12 col-md-6 col-sm-12 shadow pt-1'>                        
                         <div className='row'>                            
                             <div className='col-12 col-md-6 col-sm-12 mt-3'>
-                                <img className='w-100 batch-card card-img-top' src="https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg"></img>
+                                <img className='batch-card card-img-top' loading='lazy' alt='uploading' src={`../teacher/${teacher.image}`}/>                                
                             </div>
                             <div className='col-12 col-md-6 col-sm-12 mt-2'>
                                 <label><MdMarkEmailRead/> </label> &nbsp;
