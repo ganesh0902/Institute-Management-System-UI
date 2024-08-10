b0fa4353a04dc4f40c26ee3af1d5ae562ad96b93
Ganesh sakhare <101263428+ganeshsakhare0902@users.noreply.github.com>
2024-08-10 22:38:11 +0530

    changes

--- institute/src/component/teacherComponent/Batches.js
+++ institute/src/component/teacherComponent/Batches.js
@@ -3,6 +3,7 @@ import { TabView, TabPanel } from 'primereact/tabview';
 import {getBatchesByTeacher} from '..//..//apis/batchApis'
 import a from '..//..//images/back1.jpg';
 import './css/THomePage.css'
+import { NavLink } from 'react-router-dom';
  const  Batches=({instituteId,teacherId})=> {
 
     const[batches,setBatches]=useState([]);
@@ -49,56 +50,115 @@ import './css/THomePage.css'
                 }
                 acc[status].push(batch);
                 return acc;
-            }, {});
-            console.log(typeof groupedBatches); // Should log 'object'        
-            setGroupedBatches(JSON.stringify(groupedData, null, 2));   
-            console.log(groupedBatches);
+            }, {});            
+            setGroupedBatches(JSON.stringify(groupedData, null, 2));               
         }     
         groupBatchesSet();
     }, [batches]);
 
-    useEffect(()=>{
-        console.log("Group Batches is ");
-        console.log(groupedBatches);        
-        setPendingBatches(groupedBatches.started);
-        console.log("Batch exist");        
-    },[groupedBatches]);  
+    useEffect(()=>{        
+        console.log("Group Data");        
+        console.log(groupedBatches);                        
+    },[groupedBatches]);      
     
-    useEffect(()=>{
+    if(!groupedBatches)
+    {
+        return(<div> Loading</div>)
+    }
 
-        console.log("Pending batches is ");
-        console.log(pendingBatches);
-    },[pendingBatches]);
-    
-    
+    const filteredBatches = batches.filter(batch => batch.status === "End");
+    const filteredBatchStarted = batches.filter(batch => batch.status === "Started");
+    const filteredBatchPending = batches.filter(batch => batch.status === "Pending");
   return (
     <div style={linkStyle}>
         <div className='mt-3'>
         <TabView className="fixed-tab-view-for-batches">            
             <TabPanel header="Current Batch" className="batch-in-teacher-header px-4 py-2 text-5" leftIcon="pi pi-user mr-2">
-            <div className='row'>     
-            {batches.map((batch, index) => (
-                 <div className='col-12 col-sm-12 col-md-3 shadow rounder'>
-                 <div className='image'>
-                 <img className='batch-card card-img-top' loading='lazy' alt='uploading' src={a}/>
-                 </div>
-                 <div className='batchDetails'>                            
-                     <div className='title'>
-                         <label className='text-left'> Java Developer </label>   
-                     </div>
-                     <span className='text-right'>09-09-2001</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;    
-                     <span> 09-02-2021</span><br/>
-                     <span className='' style={{marginTop:"10px !important"}}> Status : Pending</span>
-                 </div>
-             </div>                                                                        
+            <div className='row'>   
+              {
+                filteredBatchStarted.length ===0 && 
+                <div className='text-center'>No Batch Upcoming Found</div>
+              }    
+            {batches
+                .filter(batch => batch.status === "Started").map((batch, index) => (            
+                <div className='col-12 col-sm-12 col-md-3 shadow rounder'>
+                    <NavLink to="" className="navLink">
+                    <div className='image'>
+                    <img className='batch-card card-img-top' loading='lazy' alt='uploading' src={a}/>
+                    </div>
+                    <div className='batchDetails'>                            
+                        <div className='title'>
+                            <label className='text-left text-dark'> {batch.batchTitle} </label>   
+                        </div>
+                        <div className='batchInfo'>
+                            <span> {batch.startDate}</span>
+                            <span> {batch.endDate} </span>
+                            <span>Time {batch.time} </span>
+                            <span className={batch.status} style={{marginTop:"10px !important"}}> Status : {batch.status}</span>
+                        </div>    
+                    </div>
+                    </NavLink>
+                </div>                        
             ))}           
             </div>
             </TabPanel>
             <TabPanel header="Up Coming Batch" className="batch-in-teacher-header px-4 py-2 text-5" leftIcon="pi pi-user mr-2">
-                 <h2> Up Coming Batch</h2>   
+            <div className='row'>
+            {
+                filteredBatchPending.length ===0 && 
+                <div className='text-center'> No Upcoming Batch found</div>
+            }    
+                {batches
+                    .filter(batch => batch.status === "Pending").map((batch, index) => (
+                    <div className='col-12 col-sm-12 col-md-3 shadow rounder'>
+                        <NavLink to="" className="navLink">
+                        <div className='image'>
+                            <img className='batch-card card-img-top' loading='lazy' alt='uploading' src={a}/>
+                        </div>
+                        <div className='batchDetails'>                            
+                            <div className='title'>
+                                <label className='text-left text-dark'> {batch.batchTitle} </label>   
+                            </div>
+                            <div className='batchInfo'>
+                                <span> {batch.startDate}</span>
+                                <span> {batch.endDate} </span>
+                                <span>Time: {batch.time} </span>
+                                <span className={batch.status} style={{marginTop:"10px !important"}}> Status : {batch.status}</span>
+                            </div>                                
+                        </div>
+                        </NavLink>
+                    </div>                                                                                            
+                ))}        
+                </div>  
             </TabPanel>
-            <TabPanel header="End Batches" className="batch-in-teacher-header px-4 py-2 text-5" leftIcon="pi pi-user mr-2">
-                 <h2> End Batch</h2>   
+            <TabPanel header="End Batches" className="batch-in-teacher-header px-4 py-2 text-5" leftIcon="pi pi-user mr-2">                
+            <div className='row'>   
+            {
+                filteredBatches.length ===0 && 
+                <div className='text-center'> No Data Found </div>            
+            }    
+            {batches
+                .filter(batch => batch.status === "End").map((batch, index) => (
+                <div className='col-12 col-sm-12 col-md-3 shadow rounder'>
+                    <NavLink to="" className="navLink">
+                    <div className='image'>
+                        <img className='batch-card card-img-top' loading='lazy' alt='uploading' src={a}/>
+                    </div>
+                    <div className='batchDetails'>                            
+                        <div className='title'>
+                            <label className='text-dark text-center'> {batch.batchTitle} </label>   
+                        </div>
+                        <div className='batchInfo'>
+                            <span> {batch.startDate}</span>
+                            <span> {batch.endDate} </span><br/>
+                            <span>Time {batch.time} </span>
+                            <span className={batch.status} style={{marginTop:"10px !important"}}> Status : {batch.status}</span>
+                        </div>    
+                    </div>
+                    </NavLink>
+                </div>                                                                        
+            ))}           
+            </div>                
             </TabPanel>
         </TabView>
         </div>
