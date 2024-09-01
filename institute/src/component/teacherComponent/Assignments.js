3b5aff2c1412e36bc986779344162e7b93c822bf
Ganesh sakhare <101263428+ganeshsakhare0902@users.noreply.github.com>
2024-09-01 14:47:51 +0530

    changes for teacher assignment

--- institute/src/component/teacherComponent/Assignments.js
+++ institute/src/component/teacherComponent/Assignments.js
@@ -5,6 +5,7 @@ import {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom';
 import { getAssignmentByBatchIdAPI } from '../../apis/assignment';
 import Loader from '../Loader';
+import { Accordion, AccordionTab } from 'primereact/accordion';
 const Assignments=()=>{
     
     const[assignment,setAssignment]=useState([]);
@@ -52,30 +53,41 @@ const Assignments=()=>{
     {
         return(<Loader/>)
     }
+
+    const assignmentListCss={
+        padding: '15px 15px 15px 0px', 
+        marginTop: '10px', 
+        display: 'flex', 
+        justifyContent: 'space-between', 
+        alignItems: 'center'
+    }
     return(
         <div style={linkStyle}> 
         <div className='mt-3'>
         <TabView className="fixed-tab-view-for-batches"> 
-        <TabPanel header="Active Assignment" className="batch-in-teacher-header px-4 py-2 text-5" leftIcon="pi pi-user mr-2">
+        <TabPanel header="Active Assignment" className="batch-in-teacher-header px-4 text-5" leftIcon="pi pi-user mr-2">
                     <div className='row'>
                         {
                             filterAssignmentActive.length ===0 && <div className='text-center'> No Active Assignment Found </div>
-                        }
+                        }                       
+                        <div className='col-12 col-sm-12 col-md-12 shadow assignment-card'>
+                        <div className='assignment-list'>
                         {
                             assignment.filter(assignment=> assignment.status ==='active').map((assignment,index)=>(
-                            <div className='col-12 col-sm-12 col-md-3 shadow assignment-card'>
-                            <NavLink to="">
-                                <div className=''>
-                                    <h4>{assignment.title}</h4>                               
-                                    <p>Write a Java program that creates or appends to a log file, 
-                                        adding a new entry each time the program is run. Each entry 
-                                        should be timestamped with the current date and time.</p>
-                                    <small>02-JUN-2024</small>    
+                            <Accordion activeIndex={-1}>
+                            <AccordionTab header={
+                                <div style={assignmentListCss}>
+                                    <span>{assignment.title}</span>
+                                    <small>09-02-2001</small>
                                 </div>
-                            </NavLink>
-                        </div>                                                        
+                                }>
+                                <div> Hello </div> 
+                                </AccordionTab>
+                            </Accordion>
                             ))
-                        }                                                
+                        }     
+                        </div>   
+                        </div>                                                                                                                           
                     </div>
             </TabPanel>                  
             <TabPanel header="Pending Assignment" className="batch-in-teacher-header px-4 py-2 text-5" leftIcon="pi pi-user mr-2">
